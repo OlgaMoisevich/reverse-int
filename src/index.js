1545b9a5ff2d072b58588e94f458878fc636610e
OlgaMoisevich <ol.ivochka@yandex.ru>
2021-03-11 16:07:34 +0300

    feat: add the solution task reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    if (n > 0) {
+        let arr = String(n).split('').reverse().join('');
+        return Number(arr);
+    } else if (n < 0) {
+        let arr = String(n).split('').reverse();
+        let el_del = arr.pop();
+        return Number(arr.join(''));
+    }
 }
